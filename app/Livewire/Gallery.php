5c82a43fd255fb8a5240ebdc75f66c20606508b9
joao.vdsm <joao.vdsmg@gmail.com>
2024-03-07 13:11:14 -0300

    Featured image

--- app/Livewire/Gallery.php
+++ app/Livewire/Gallery.php
@@ -6,11 +6,19 @@ use Livewire\Component;
 
 class Gallery extends Component
 {
+    public $images;
+    public $featuredUrl;
+
     public function mount($images) {
-        dd($images);
+        $this->images = $images;
+        $this->featuredUrl = $images->where('featured', true)->first()->url;
     }
     
     public function render() {
         return view('livewire.gallery');
+    }
+
+    public function setFeatured(String $url) {
+        $this->featuredUrl = $url;
     }
 }
