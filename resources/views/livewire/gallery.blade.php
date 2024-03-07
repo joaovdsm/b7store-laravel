766d3ff8e54f3ebd3de57de3e352481686e21f70
joao.vdsm <joao.vdsmg@gmail.com>
2024-03-07 13:28:47 -0300

    Add opacity and transition effect in gallery

--- resources/views/livewire/gallery.blade.php
+++ resources/views/livewire/gallery.blade.php
@@ -1,8 +1,12 @@
 <div class="ad-area-left">
-    <div class="main-photo" style="background-image: url({{ $featuredUrl }})"></div>
+    <div class="main-photo" style="background-image: url({{ $featuredUrl }});"></div>
     <div class="secundary-photos">
       @foreach ($images as $image)
-        <div wire:click="setFeatured('{{ $image->url }}')" class="secundary-image" style="background-image: url({{ $image->url }})"></div>
+        <div wire:click="setFeatured('{{ $image->url }}')" class="secundary-image" 
+          style="background-image: url({{ $image->url }}); 
+          opacity: {{ $image->url === $featuredUrl ? 1 : 0.5 }};
+          transition: all .5s;"
+        ></div>
       @endforeach
     </div>
 </div>
